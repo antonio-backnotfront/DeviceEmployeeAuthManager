00d8fa898e05d3d679b0f294deb3ea59474567c6
Anton Solianyk <solyanicks@gmail.com>
2025-06-04 10:59:57 +0200

    Add regexes to CreateAccountDto

--- src/DeviceEmployeeAuthManager.DTO/CreateAccountDto.cs
+++ src/DeviceEmployeeAuthManager.DTO/CreateAccountDto.cs
@@ -6,8 +6,11 @@ public class CreateAccountDto
 {
     public int Id;
     [Required]
+    [RegularExpression(@"^[^\d]\w*", ErrorMessage = "Username must not start with a number.")]
     public string Username { get; set; }
     [Required]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,}$",
+        ErrorMessage = "Password must be at least 12 characters long and include one lowercase letter, one uppercase letter, one number, and one special character.")]
     public string Password { get; set; }
     [Required]
     public int EmployeeId { get; set; }
